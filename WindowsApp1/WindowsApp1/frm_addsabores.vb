9165e4fa9f8203aae9d15e4e8d2cde4e4896819b
gabrielgimenez98 <33259939+gabrielgimenez98@users.noreply.github.com>
2017-11-20 12:49:44 -0200

    Inserindo funcionários e novos sabores

--- WindowsApp1/WindowsApp1/frm_addsabores.vb
+++ WindowsApp1/WindowsApp1/frm_addsabores.vb
@@ -1,4 +1,5 @@
 ﻿Public Class frm_addsabores
+    Dim id As Integer
     Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
 
     End Sub
@@ -8,22 +9,50 @@
             MsgBox("Campos em branco", vbInformation + vbOKOnly, "Atenção")
         Else
             If cmb_tipo.SelectedIndex = (0) Then
-                sql = "insert into tb_pizzas (nome_pizza,preco_pizza,descricao_pizza) values ('" & txt_nome.Text & "', " & txt_valor.Text & ", '" & txt_descricao.Text & "')"
+                Verifica_Pizza()
+                sql = "insert into tb_pizzas (id_pizza,nome_pizza,preco_pizza,descricao_pizza) values (" & id & " , '" & txt_nome.Text & "' , '" & txt_valor.Text & "' , '" & txt_descricao.Text & "')"
                 rs = db.Execute(sql)
                 MsgBox("nova pizza cadastrada!")
+                limpar()
             Else
-                sql = "insert into tb_bordas(nome_borda,preco_borda,descricao_borda) values ('" & txt_nome.Text & "', " & txt_valor.Text & ", '" & txt_descricao.Text & "')"
+                Verifica_Borda()
+                sql = "insert into tb_bordas(id_borda,nome_borda,preco_borda,descricao_borda) values (" & id & " , '" & txt_nome.Text & "' , '" & txt_valor.Text & "' , '" & txt_descricao.Text & "')"
                 rs = db.Execute(sql)
                 MsgBox("nova borda cadastrada!")
+                limpar()
             End If
         End If
         limpar()
     End Sub
 
     Private Sub frm_addsabores_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conecta_banco()
+
         With cmb_tipo.Items
             .Add("Pizza")
             .Add("Borda")
         End With
+    End Sub
+    Sub Verifica_Pizza()
+        sql = "select * from tb_pizzas"
+        rs = db.Execute(sql)
+        If rs.BOF = True Then
+            id = 1
+        Else
+            sql = "select MAX (id_pizza) as id from tb_pizzas"
+            rs = db.Execute(sql)
+            id = rs.Fields(0).Value + 1
+        End If
+    End Sub
+    Sub Verifica_Borda()
+        sql = "select * from tb_bordas"
+        rs = db.Execute(sql)
+        If rs.BOF = True Then
+            id = 1
+        Else
+            sql = "select MAX (id_borda) as id from tb_bordas"
+            rs = db.Execute(sql)
+            id = rs.Fields(0).Value + 1
+        End If
     End Sub
 End Class
--- WindowsApp1/WindowsApp1/frm_adduser.vb
+++ WindowsApp1/WindowsApp1/frm_adduser.vb
@@ -8,6 +8,7 @@
     End Sub
 
     Private Sub frm_adduser_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conecta_banco()
         With cmb_cargo.Items
             .Add("Vendedor")
             .Add("Entregador")
@@ -19,6 +20,11 @@
     Private Sub btn_confirmar_Click(sender As Object, e As EventArgs) Handles btn_confirmar.Click
         If txt_nome.Text = "" Or txt_cpf.Text = "" Or txt_data.Text = "" Or txt_senha.Text = "" Then
             MsgBox("Campos em branco", vbInformation + vbOKOnly, "Atenção")
+        Else
+            sql = "insert into tb_login values('" & txt_cpf.Text & "' , '" & txt_nome.Text & "' , '" & txt_senha.Text & "' , '" & cmb_cargo.Text & "', '" & txt_data.Text & "' , '', 'Ativo')"
+            rs = db.Execute(sql)
+            MsgBox("Funcionário cadastrado!!!")
+            limpar()
         End If
     End Sub
 End Class
